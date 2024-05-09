38b3746a2a67f6665180d0600e44d60446b538d8
Nick Ochieng <1nickochieng@gmail.com>
2024-05-09 11:14:54 +0300

    changes

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import {Header} from "./";
 
 const App = () => {
   return (
-    <div className="flex item-center justify-center">
-      <h1>Hi there</h1>
-    </div>
+    <div className="w-full xl:w- [1600px] py-32 px-4 lg:12 pr-4 lg:pr-32">
+
+
+{/* particles container */}
+
+{/* header */}
+<Header />
+{/* home container */}
+
+{/* about */}
+
+{/* skills */}
+
+{/* contact */}
+
+{/* footer */}
+
+
+</div>
   );
 };
 
